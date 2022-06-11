53a3a5ec83a8ca3d5cd783e7eefce4fdeb18a954
YSSeah99 <yusiangseah99@gmail.com>
2022-06-12 01:09:23 +0800

    Updated Security Features

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+from secret import secretpassword
 
 app = Flask(__name__)
+app.secret_key = secretpassword
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.after_request
@@ -26,7 +28,44 @@ def helpers():
     time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     return render_template("helpers.html", time=time)
 
-@app.route("/helperform",methods=["GET"])
+@app.route("/helperform",methods=["GET","POST"])
 def helperform():
-    time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    return render_template("helperform.html", time=time)
+
+    if request.method == "POST":
+        
+        if request.form.get("passwordone") != request.form.get("passwordtwo"):
+            flash("Password do not match")
+            return redirect("/helperform")
+        
+        else:
+            password = request.form.get("passwordtwo")
+            if len(password) < 8:
+                flash("Password must be at least 8 characters long")
+                return redirect("/helperform")
+            
+            else:
+                password = generate_password_hash
+                email = request.form.get("email")
+                if "@u.nus.edu" not in email:
+                    flash("Invalid email address")
+                    return redirect("/helperform")
+                
+                else:
+                    -
+                    # TODO: checks database of helpers if name is already taken (If/Else check)
+                    name = request.form.get("name")
+                    telegram = request.form.get("telegram")
+                    if request.form.get("github"):
+                        github = request.form.get("github")
+                    else:
+                        github = ""
+                    time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                    return render_template("helperform.html", time=time)              
+    
+    else:
+        time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        return render_template("helperform.html", time=time)  
+    
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
