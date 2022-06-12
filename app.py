50dd81c318acd5fe1a0fe18aee8c2eb9c533ea79
YSSeah99 <yusiangseah99@gmail.com>
2022-06-12 14:35:48 +0800

    Created security page to register helpers

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 
+from cs50 import SQL
 from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, session
 from tempfile import mkdtemp
@@ -9,6 +10,7 @@ from secret import secretpassword
 app = Flask(__name__)
 app.secret_key = secretpassword
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+db = SQL("sqlite:///planner.db")
 
 @app.after_request
 def after_request(response):
@@ -30,38 +32,50 @@ def helpers():
 
 @app.route("/helperform",methods=["GET","POST"])
 def helperform():
-
+    
     if request.method == "POST":
         
-        if request.form.get("passwordone") != request.form.get("passwordtwo"):
+        password = request.form.get("passwordtwo")
+        email = request.form.get("email")
+        name = request.form.get("name")
+        telegram = request.form.get("telegram")
+        
+        if request.form.get("github"):
+            github = request.form.get("github")
+            
+        else:
+            github = ""
+     
+        if request.form.get("passwordone") != password:
             flash("Password do not match")
             return redirect("/helperform")
         
-        else:
-            password = request.form.get("passwordtwo")
-            if len(password) < 8:
+        elif len(password) < 8:
                 flash("Password must be at least 8 characters long")
                 return redirect("/helperform")
             
+        else:
+            hashed = generate_password_hash(password)
+                
+            if "@u.nus.edu" not in email:
+                flash("Invalid email address")
+                return redirect("/helperform")
+                
             else:
-                password = generate_password_hash
-                email = request.form.get("email")
-                if "@u.nus.edu" not in email:
-                    flash("Invalid email address")
+                
+                helperformDB = db.execute("SELECT * FROM Helpersform WHERE email = ?", email)
+                helperDB = db.execute("SELECT * FROM Helpers WHERE email = ?", email) 
+                # TODO: checks database of helpers if name is already taken (If/Else check)
+                
+                if len(helperformDB) == 1 or len(helperDB) == 1:
+                    flash("You already have an account registered!")
                     return redirect("/helperform")
                 
                 else:
-                    -
-                    # TODO: checks database of helpers if name is already taken (If/Else check)
-                    name = request.form.get("name")
-                    telegram = request.form.get("telegram")
-                    if request.form.get("github"):
-                        github = request.form.get("github")
-                    else:
-                        github = ""
-                    time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                    return render_template("helperform.html", time=time)              
-    
+                    db.execute("INSERT INTO Helpersform (name, email, telegram, github, hash) VALUES (?, ?, ?, ?, ?)", name, email, telegram, github, hashed)
+                    flash("You have successfully registered!")
+                    return redirect("/helpers")
+            
     else:
         time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         return render_template("helperform.html", time=time)  
